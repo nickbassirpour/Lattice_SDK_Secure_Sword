8c8a86461bc6776198b541e5f2d421762e56f149
nickbassirpour <nickbassirpour@gmail.com>
2025-04-26 21:13:17 -0700

    ADding sometihng

--- core/entities/location.go
+++ core/entities/location.go
@@ -1,5 +1,6 @@
 package entities
 
+// Available for Entities that have a single or primary Location.
 type Location struct {
 	Position types.Position `json:"position"`
 }
--- core/entities/status.go
+++ core/entities/status.go
@@ -1,7 +1,22 @@
 package entities
 
+// The Status type defines a logical error model that is
+// suitable for different programming environments, including
+// REST APIs and RPC APIs.
 type Status struct {
+
+	// The status code, which should be an enum value
+	// of [google.rpc.Code][google.rpc.Code].
 	Code int32 `json:"code"`
+
+	// A developer-facing error message, which should be in English. Any user-facing error message should be
+	// localized and sent in the [google.rpc.Status.details][google.rpc.Status.details] field, or localized by the client.
 	Message string `json:"message"`
-	Details string[] `json:"details"`
+
+	// A list of messages that carry the error details. There is a common set of message types for APIs to use.
+	Details []StatusDetail `json:"details"`
+}
+
+type StatusDetail struct {
+	Type string `json:"@type"`
 }
